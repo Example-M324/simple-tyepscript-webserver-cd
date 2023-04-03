dd17de982d7518abd88f5497603c5d10403ebe8f
switzerchees <patrickmichel96@yahoo.de>
2023-04-03 15:32:52 +0200

    Add endpoint for /log/time

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,14 @@ app.get('/', (req: Request, res: Response) => {
   res.send(`<h1>Hello CSBE!</h1><p>Host: ${os.hostname()}</p>`);
 });
 
+// route for /log/time that logs the current time to the console and returns the time
+app.get('/log/time', (req: Request, res: Response) => {
+  const time = new Date().toISOString();
+  // eslint-disable-next-line no-console
+  console.log(`Request received at ${time}`);
+  res.send(time);
+});
+
 const serverPort = process.env.PORT || 3000;
 server.listen(serverPort, () => {
   // eslint-disable-next-line no-console
